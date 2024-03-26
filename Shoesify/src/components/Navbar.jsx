28dce8231335b91427792b661dbc45df15f56e87
TusharSanisare <121483380+TusharSanisare@users.noreply.github.com>
2024-03-27 01:09:45 +0530

    ui

--- Shoesify/src/components/Navbar.jsx
+++ Shoesify/src/components/Navbar.jsx
@@ -17,34 +17,36 @@ const Navbar = () => {
               id=""
             >
               <option>all categories</option>
+              <option>all categories</option>
+              <option>all categories</option>
+              <option>all categories</option>
+              <option>all categories</option>
+              <option>all categories</option>
+              <option>all categories</option>
+              <option>all categories</option>
             </select>
             <input
               className="border-l border-gray-300 bg-transparent font-semibold text-sm pl-4"
               type="text"
               placeholder="I'm searching for ..."
             />
-            <svg
-              className="ml-auto h-5 px-4 text-gray-500"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="far"
-              data-icon="search"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <path
-                fill="currentColor"
-                d="M508.5 468.9L387.1 347.5c-2.3-2.3-5.3-3.5-8.5-3.5h-13.2c31.5-36.5 50.6-84 50.6-136C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c52 0 99.5-19.1 136-50.6v13.2c0 3.2 1.3 6.2 3.5 8.5l121.4 121.4c4.7 4.7 12.3 4.7 17 0l22.6-22.6c4.7-4.7 4.7-12.3 0-17zM208 368c-88.4 0-160-71.6-160-160S119.6 48 208 48s160 71.6 160 160-71.6 160-160 160z"
-              />
-            </svg>
+            <i className="ml-auto px-4 text-gray-500 text-2xl ri-search-line"></i>
           </div>
-          {/* phone number */}
+
+          {/* SignUp Login Buttons */}
+          {/* <div className="ml-auto md:w-48 hidden sm:flex flex-row gap-2 place-items-end">
+            <button class="bg-transparent text-gray-800 hover:text-black font-semibold py-2 px-4 border hover:border-black border-gray-800 rounded">
+              SignUp
+            </button>
+            <button class="bg-transparent text-gray-800 hover:text-black font-semibold py-2 px-4 border hover:border-black border-gray-800 rounded">
+              Login
+            </button>
+          </div> */}
+          {/* for logged in user */}
           <div className="ml-auto md:w-48 hidden sm:flex flex-col place-items-end">
             <span className="font-semibold text-sm text-gray-400">User</span>
             <span className="font-bold md:text-xl">Rohan paul</span>
           </div>
-          {/* buttons */}
           <nav className="contents">
             <ul className="ml-4 xl:w-48 flex items-center justify-end">
               <li className="ml-2 lg:ml-4 relative inline-block">
